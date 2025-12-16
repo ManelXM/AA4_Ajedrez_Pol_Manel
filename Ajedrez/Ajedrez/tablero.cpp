57f0e0e73de47d39345af45cd237f25e736953f9
Araezz <pol.araez@estudiant.enti.cat>
2025-12-16 12:23:35 +0100

    Update tablero.cpp

--- Ajedrez/Ajedrez/tablero.cpp
+++ Ajedrez/Ajedrez/tablero.cpp
@@ -1,37 +1,59 @@
 #include <iostream>
 
-#define FILAS 10
-#define COLUMNAS 10
-#define NUMEROS 9
+#define FILAS 8
+#define COLUMNAS 8
 
-void tablero();
+void piezas(char tablero[FILAS][COLUMNAS]) {
 
-int main() {
-    tablero();
-    return 0;
+    for (int i = 0; i < FILAS; i++)
+        for (int j = 0; j < COLUMNAS; j++)
+            tablero[i][j] = '*';
+
+    tablero[0][0] = 't';
+    tablero[0][1] = 'h';
+    tablero[0][2] = 'b';
+    tablero[0][3] = 'k';
+    tablero[0][4] = 'q';
+    tablero[0][5] = 'b';
+    tablero[0][6] = 'h';
+    tablero[0][7] = 't';
+
+    for (int j = 0; j < 8; j++)
+        tablero[1][j] = 'p';
+
+    tablero[7][0] = 't';
+    tablero[7][1] = 'h';
+    tablero[7][2] = 'b';
+    tablero[7][3] = 'k';
+    tablero[7][4] = 'q';
+    tablero[7][5] = 'b';
+    tablero[7][6] = 'h';
+    tablero[7][7] = 't';
+
+    for (int j = 0; j < 8; j++)
+        tablero[6][j] = 'P';
 }
 
-void tablero() {
+void tableroPrint(char tablero[FILAS][COLUMNAS]) {
+
+    std::cout << "   ";
+    for (int i = 0; i < COLUMNAS; i++)
+        std::cout << i + 1 << " ";
+    std::cout << "\n";
+
     for (int i = 0; i < FILAS; i++) {
-        for (int x = 0; x < COLUMNAS; x++) {
-
-            if (i == 0 || i == FILAS - 1) {
-                std::cout << " -";
-            }
-            else if (x == 0) {
-                std::cout << "|";
-            }
-            else if (x == COLUMNAS - 2) {
-                std::cout << "-|";
-            }
-            else if (x == COLUMNAS - 1) {
-                std::cout << i;
-            }
-            else {
-                std::cout << "-|";
-            }
-
-        }
+        std::cout << i + 1 << "  ";
+        for (int j = 0; j < COLUMNAS; j++)
+            std::cout << tablero[i][j] << " ";
         std::cout << "\n";
     }
+}
+
+int main() {
+    char tablero[FILAS][COLUMNAS];
+
+    piezas(tablero);
+    tableroPrint(tablero);
+
+    return 0;
 }
